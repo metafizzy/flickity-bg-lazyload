b2571ddfd810b7a49338999741551f78c204185b
Dave DeSandro <desandrocodes@gmail.com>
2022-03-08 20:13:10 -0500

    🔔 v2.0.0 for Flickity v3
    - ES2018 syntax
    - add lint with ESLint
    - switch to Flickity.create
    - add package.json

--- bg-lazyload.js
+++ bg-lazyload.js
@@ -1,83 +1,70 @@
 /**
- * Flickity background lazyload v1.0.1
+ * Flickity background lazyload v2.0.0
  * lazyload background cell images
  */
 
-/*jshint browser: true, unused: true, undef: true */
-
 ( function( window, factory ) {
   // universal module definition
-  /*globals define, module, require */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD
-    define( [
-      'flickity/js/index',
-      'fizzy-ui-utils/utils'
-    ], factory );
-  } else if ( typeof module == 'object' && module.exports ) {
+  if ( typeof module == 'object' && module.exports ) {
     // CommonJS
     module.exports = factory(
-      require('flickity'),
-      require('fizzy-ui-utils')
+        require('flickity'),
+        require('fizzy-ui-utils'),
     );
   } else {
     // browser global
     factory(
-      window.Flickity,
-      window.fizzyUIUtils
+        window.Flickity,
+        window.fizzyUIUtils,
     );
   }
 
 }( window, function factory( Flickity, utils ) {
-/*jshint strict: true */
-'use strict';
 
-Flickity.createMethods.push('_createBgLazyLoad');
-
-var proto = Flickity.prototype;
-
-proto._createBgLazyLoad = function() {
+Flickity.create.bgLazyLoad = function() {
   this.on( 'select', this.bgLazyLoad );
 };
 
+let proto = Flickity.prototype;
+
+const bgLazyloadAttr = 'data-flickity-bg-lazyload';
+
 proto.bgLazyLoad = function() {
-  var lazyLoad = this.options.bgLazyLoad;
-  if ( !lazyLoad ) {
-    return;
-  }
+  let lazyLoad = this.options.bgLazyLoad;
+  if ( !lazyLoad ) return;
 
   // get adjacent cells, use lazyLoad option for adjacent count
-  var adjCount = typeof lazyLoad == 'number' ? lazyLoad : 0;
-  var cellElems = this.getAdjacentCellElements( adjCount );
+  let adjCount = typeof lazyLoad == 'number' ? lazyLoad : 0;
+  let cellElems = this.getAdjacentCellElements( adjCount );
 
-  for ( var i=0; i < cellElems.length; i++ ) {
-    var cellElem = cellElems[i];
+  cellElems.forEach( ( cellElem ) => {
     this.bgLazyLoadElem( cellElem );
     // select lazy elems in cell
-    var children = cellElem.querySelectorAll('[data-flickity-bg-lazyload]');
-    for ( var j=0; j < children.length; j++ ) {
-      this.bgLazyLoadElem( children[j] );
+    let children = cellElem.querySelectorAll(`[${bgLazyloadAttr}]`);
+    for ( let child of children ) {
+      this.bgLazyLoadElem( child );
     }
-  }
+  } );
 };
 
 proto.bgLazyLoadElem = function( elem ) {
-  var attr = elem.getAttribute('data-flickity-bg-lazyload');
-  if ( attr ) {
-    new BgLazyLoader( elem, attr, this );
-  }
+  let attr = elem.getAttribute( bgLazyloadAttr );
+  if ( !attr ) return;
+
+  let onComplete = ( event ) => {
+    this.dispatchEvent( 'bgLazyLoad', event, elem );
+  };
+  new BgLazyLoader( elem, attr, onComplete );
 };
 
 // -------------------------- LazyBGLoader -------------------------- //
 
-/**
- * class to handle loading images
- */
-function BgLazyLoader( elem, url, flickity ) {
+// class to handle loading images
+function BgLazyLoader( elem, url, onComplete ) {
   this.element = elem;
   this.url = url;
   this.img = new Image();
-  this.flickity = flickity;
+  this.onComplete = onComplete;
   this.load();
 }
 
@@ -89,11 +76,11 @@ BgLazyLoader.prototype.load = function() {
   // load image
   this.img.src = this.url;
   // remove attr
-  this.element.removeAttribute('data-flickity-bg-lazyload');
+  this.element.removeAttribute( bgLazyloadAttr );
 };
 
 BgLazyLoader.prototype.onload = function( event ) {
-  this.element.style.backgroundImage = 'url("' + this.url + '")';
+  this.element.style.backgroundImage = `url("${this.url}")`;
   this.complete( event, 'flickity-bg-lazyloaded' );
 };
 
@@ -107,7 +94,7 @@ BgLazyLoader.prototype.complete = function( event, className ) {
   this.img.removeEventListener( 'error', this );
 
   this.element.classList.add( className );
-  this.flickity.dispatchEvent( 'bgLazyLoad', event, this.element );
+  this.onComplete( event );
 };
 
 // -----  ----- //
@@ -116,4 +103,4 @@ Flickity.BgLazyLoader = BgLazyLoader;
 
 return Flickity;
 
-}));
+} ) );
--- test/test-bg-lazyload.js
+++ test/test-bg-lazyload.js
@@ -1,16 +1,13 @@
-/*jshint browser: true, undef: true, strict: true, unused: true */
-/*globals QUnit, Flickity */
 QUnit.test( 'bgLazyLoad', function( assert ) {
-  'use strict';
 
-  var done = assert.async();
+  let done = assert.async();
 
-  var carousel = document.querySelector('.carousel--bg-lazyload')
-  var flkty = new Flickity( carousel, {
-    bgLazyLoad: 1
-  });
+  let carousel = document.querySelector('.carousel--bg-lazyload');
+  let flkty = new Flickity( carousel, {
+    bgLazyLoad: 1,
+  } );
 
-  var loadCount = 0;
+  let loadCount = 0;
   flkty.on( 'bgLazyLoad', function( event, elem ) {
     loadCount++;
 
@@ -18,14 +15,14 @@ QUnit.test( 'bgLazyLoad', function( assert ) {
     assert.ok( elem, 'elem argument there' );
 
     // after first 2 have loaded, select 7th cell
-    if ( loadCount == 2 ) {
+    if ( loadCount === 2 ) {
       flkty.select( 6 );
     }
-    if ( loadCount == 5 ) {
-      var loadedImgs = carousel.querySelectorAll('.flickity-bg-lazyloaded');
+    if ( loadCount === 5 ) {
+      let loadedImgs = carousel.querySelectorAll('.flickity-bg-lazyloaded');
       assert.equal( loadedImgs.length, '5', 'only 5 images loaded' );
       done();
     }
-  });
+  } );
 
-});
+} );
